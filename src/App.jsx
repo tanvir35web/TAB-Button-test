d6c52e3823ff3e5a45e2af5b4473e0b47362aa20
tanvir35web <islammdtanvirul@gmail.com>
2023-12-30 00:06:36 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Header from './components/Header';
+import TabMenu from './components/TabMenu';
+
+function App() {
+  
+
+  return (
+    <>
+      <Header/>
+      <TabMenu/>
+    </>
+  )
+}
+
+export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './HeaderStyle.css'
+
+export default function Header() {
+  return (
+    <>
+        <h1 className='heading'>জাভাস্ক্রিপ্ট বুটক্যাম্পের সাথে আপনার ক্যারিয়ার বুস্ট করুন</h1>
+    </>
+  )
+}
